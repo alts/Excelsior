403d9fdf2198bf7ea0d10a49166216b01f07f861
alts <stephen@evilrobotstuff.com>
2010-08-09 14:05:43 -0700

    Upon closer inspection fo the spec, Table has no required attributes. Let's drop them

--- libs/ExcelsiorTable.php
+++ libs/ExcelsiorTable.php
@@ -8,20 +8,7 @@ class ExcelsiorTable extends ExcelsiorTag
 
 	public function __toString()
 	{
-		$max_row_length = $this->attrs['ExpandedColumnCount'] = 0;
-		foreach ($this->children as $row)
-		{
-			if ($max_row_length < $row->child_count())
-			{
-				$max_row_length = $this->attrs['ExpandedColumnCount'] = $row->child_count();
-			}
-		}
-
-		$this->attrs['ExpandedRowCount'] = $this->child_count();
-		$this->attrs['FullRows'] = $this->attrs['FullColumns'] = 1;
-
 		$this->message['row_number'] = new ExcelsiorCounter();
-
 		return parent::__toString();
 	}
 }
